b108397c19882264b28d7946acc51b7da7bb03a0
KlyrhonMiko <aurelklyrhonmiko@gmail.com>
2026-08-01 06:12:39 +0800

    feat: reveal animation when coming from portfolio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,12 @@ import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import Features from "@/components/Features";
 import Footer from "@/components/Footer";
+import PortfolioReveal from "@/components/PortfolioReveal";
 
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col">
+      <PortfolioReveal />
       <Navbar />
       <main className="flex-grow">
         <Hero />
