ca5bedcd5a7a2b16c257ae4e0db3b7ed84e2c350
KlyrhonMiko <aurelklyrhonmiko@gmail.com>
2026-08-01 06:23:44 +0800

    feat: 404 error fix

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: '/koin',
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/koin',
+        basePath: false,
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
