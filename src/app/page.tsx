da15fb13c00acc97e7574c168a8d775dddb45542
KlyrhonMiko <aurelklyrhonmiko@gmail.com>
2026-09-08 04:02:26 +0800

    feat: removed on load animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,10 @@ import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import Features from "@/components/Features";
 import Footer from "@/components/Footer";
-import PortfolioReveal from "@/components/PortfolioReveal";
 
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col">
-      <PortfolioReveal />
       <Navbar />
       <main className="flex-grow">
         <Hero />
